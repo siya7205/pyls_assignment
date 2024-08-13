c5aed681e011aa07da5ddc8a63b8e1cb83449e7d
Siya Jethliya <siya@Siyas-Laptop.local>
2024-08-13 13:59:33 +0530

    Added assertions for function arguments - Resolves #8

--- pyls.py
+++ pyls.py
@@ -21,8 +21,10 @@ def list_files_with_suffixes():
         list: A list of file and directory names with appropriate suffixes.
     """
     files = os.listdir('.')
+    assert isinstance(files, list), "Expected list of files"
     result = []
     for file in files:
+        assert isinstance(file, str), "Expected file name to be a string"
         if os.path.isdir(file):
             result.append(file + '/')
         elif os.access(file, os.X_OK):
@@ -40,8 +42,10 @@ def list_files_with_details():
         list: A list of file and directory details.
     """
     files = os.listdir('.')
+    assert isinstance(files, list), "Expected list of files"
     result = []
     for file in files:
+        assert isinstance(file, str), "Expected file name to be a string"
         stats = os.stat(file)
         last_modified = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(stats.st_mtime))
         size = stats.st_size if not os.path.isdir(file) else 0
